c339b67ab3e6b5010ef35fcd29e81378ba219da9
ahmed-abdelhamid <slevin.glevra@gmail.com>
2021-02-08 19:29:33 +0300

    Tailwind: Add tailwindcss to the project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app';
-import '../styles/globals.css';
+import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
