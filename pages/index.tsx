d6b437d5a3e7b25f59377b2610388a37198b7301
ahmed-abdelhamid <slevin.glevra@gmail.com>
2021-02-11 17:31:43 +0300

    Search Icon: Add search icon as a component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Typography from '@components/atoms/Typgraphy/Typography';
 import Button from '@components/atoms/Button/Button';
+import SearchIcon from '@components/Icons/SearchIcon/SearchIcon';
 
 export default function Home() {
   return (
@@ -331,20 +332,7 @@ export default function Home() {
                 </label>
                 <div className="relative w-full text-gray-400 focus-within:text-gray-600">
                   <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none">
-                    {/* <!-- Heroicon name: search --> */}
-                    <svg
-                      className="h-5 w-5"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
+                    <SearchIcon />
                   </div>
                   <input
                     id="search_field"
