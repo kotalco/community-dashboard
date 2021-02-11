23f9ac10dab3c21bccc46cdb233db604c832cdce
ahmed-abdelhamid <slevin.glevra@gmail.com>
2021-02-11 17:40:18 +0300

    Bell Icon: Add bell icon as a component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Typography from '@components/atoms/Typgraphy/Typography';
 import Button from '@components/atoms/Button/Button';
 import SearchIcon from '@components/Icons/SearchIcon/SearchIcon';
 import Input from '@components/atoms/Input/Input';
+import BellIcon from '@components/Icons/BellIcon/BellIcon';
 
 export default function Home() {
   return (
@@ -347,22 +348,7 @@ export default function Home() {
             <div className="ml-4 flex items-center md:ml-6">
               <button className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 <span className="sr-only">View notifications</span>
-                {/* <!-- Heroicon name: bell --> */}
-                <svg
-                  className="h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                  />
-                </svg>
+                <BellIcon />
               </button>
             </div>
           </div>
