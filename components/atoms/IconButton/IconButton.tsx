ce27923b3e325eb4fd259d749b4015365040c2a4
ahmed-abdelhamid <slevin.glevra@gmail.com>
2021-02-13 16:03:22 +0300

    IconButton Component: Add prop types

--- components/atoms/IconButton/IconButton.tsx
+++ components/atoms/IconButton/IconButton.tsx
@@ -1,4 +1,8 @@
-const IconButton = ({ srOnly, children }) => {
+interface Props {
+  srOnly: string;
+}
+
+const IconButton: React.FC<Props> = ({ srOnly, children }) => {
   return (
     <button className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
       <span className="sr-only">{srOnly}</span>
