1b0ce0600e36f86b20f2e5cce5af8ca2bb0afafd
ahmed-abdelhamid <slevin.glevra@gmail.com>
2021-02-14 12:48:08 +0300

    Icon: Create open book icon compoenent

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import CubeIcon from '@components/Icons/CubeIcon/CubeIcon';
 import CursorClickIcon from '@components/Icons/CursorClickIcon/CursorClickIcon';
 import KeyIcon from '@components/Icons/KeyIcon/KeyIcon';
 import DocumentTextIcon from '@components/Icons/DocumentTextIcon/DocumentTextIcon';
+import BookIconIcon from '@components/Icons/BookOpenIcon/BookOpenIcon';
 
 export default function Home() {
   return (
@@ -96,20 +97,7 @@ export default function Home() {
                   href="#"
                   className="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
                 >
-                  <svg
-                    className="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
-                    />
-                  </svg>
+                  <BookIconIcon />
                   Address Book
                 </a>
               </nav>
