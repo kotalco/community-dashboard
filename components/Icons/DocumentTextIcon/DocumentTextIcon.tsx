8b58f0f760407a0c7d74af81ea3c7286e160dff6
ahmed-abdelhamid <slevin.glevra@gmail.com>
2021-02-14 12:44:04 +0300

    Icon: Create document text icon component

---
+++ components/Icons/DocumentTextIcon/DocumentTextIcon.tsx
@@ -0,0 +1,21 @@
+const DocumentTextIcon: React.FC = () => {
+  return (
+    // Heroicon name: document-text
+    <svg
+      className="text-gray-400 group-hover:text-gray-500 mr-3 h-6 w-6"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+      />
+    </svg>
+  );
+};
+
+export default DocumentTextIcon;
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import CloseIcon from '@components/Icons/CloseIcon/CloseIcon';
 import CubeIcon from '@components/Icons/CubeIcon/CubeIcon';
 import CursorClickIcon from '@components/Icons/CursorClickIcon/CursorClickIcon';
 import KeyIcon from '@components/Icons/KeyIcon/KeyIcon';
+import DocumentTextIcon from '@components/Icons/DocumentTextIcon/DocumentTextIcon';
 
 export default function Home() {
   return (
@@ -87,21 +88,7 @@ export default function Home() {
                   href="#"
                   className="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
                 >
-                  {/* <!-- Heroicon name: Contracts --> */}
-                  <svg
-                    className="text-gray-400 group-hover:text-gray-500 mr-3 h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-                    />
-                  </svg>
+                  <DocumentTextIcon />
                   Contracts
                 </a>
 
