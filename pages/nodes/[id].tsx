f4caccd7d65053f8370e08faf8bb9503d4b4c08e
ahmed-abdelhamid <slevin.glevra@gmail.com>
2021-02-19 19:27:33 +0300

    Stats: Create component

--- pages/nodes/[id].tsx
+++ pages/nodes/[id].tsx
@@ -1,4 +1,5 @@
 import PageDetailsHeader from '@components/molecules/PageDetailsHeader/PageDetailsHeader';
+import StatsComponent from '@components/molecules/Stats/Stats';
 import Layout from '@components/templates/Layout/Layout';
 
 const NodeDetails: React.FC = () => {
@@ -9,6 +10,17 @@ const NodeDetails: React.FC = () => {
           nodeName="my-etheruen-node"
           date="January 11, 2021"
         />
+
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+          {/* Stats */}
+          <div>
+            <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+              <StatsComponent title="Last Block Number" content="11,971,897" />
+              <StatsComponent title="Transactions Today" content="1,024,569" />
+              <StatsComponent title="Connected Peers" content="21" />
+            </dl>
+          </div>
+        </div>
       </div>
     </Layout>
   );
