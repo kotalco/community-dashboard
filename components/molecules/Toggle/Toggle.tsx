61c9d203ffc800a237676ea8be4e54c25dbe12a6
ahmed-abdelhamid <slevin.glevra@gmail.com>
2021-02-20 20:14:19 +0300

    Toogle: Create component

---
+++ components/molecules/Toggle/Toggle.tsx
@@ -0,0 +1,37 @@
+interface Props {
+  id: string;
+  label: string;
+  checked: boolean;
+  onClick: () => void;
+}
+
+const Toggle: React.FC<Props> = ({ id, label, checked, onClick }) => {
+  return (
+    <>
+      <label htmlFor={id} className="block text-sm font-medium text-gray-700">
+        {label}
+      </label>
+      <div className="mt-1">
+        <button
+          onClick={onClick}
+          id={id}
+          type="button"
+          aria-pressed={checked}
+          className={`${
+            checked ? 'bg-indigo-600' : 'bg-gray-200'
+          } relative inline-flex flex-shrink h-6 w-11 border-2 border-trasnparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+        >
+          <span className="sr-only">Use setting</span>
+          <span
+            aria-hidden="true"
+            className={`${
+              checked ? 'translate-x-5' : 'translate-x-0'
+            } pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200`}
+          ></span>
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Toggle;
--- components/organisms/NetworkingTabContent/NetworkingTabContent.tsx
+++ components/organisms/NetworkingTabContent/NetworkingTabContent.tsx
@@ -1,6 +1,11 @@
-import TextInputWithButton from '@components/molecules/TextInputWithButton/TextInputWithButton';
+import { useState } from 'react';
 
-const NetworkingTabContent = () => {
+import TextInputWithButton from '@components/molecules/TextInputWithButton/TextInputWithButton';
+import Toggle from '@components/molecules/Toggle/Toggle';
+
+const NetworkingTabContent: React.FC = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
   return (
     <div>
       <div className="px-4 py-5 sm:p-6">
@@ -10,6 +15,14 @@ const NetworkingTabContent = () => {
           name="nodeUrl"
           value="enode://6f8a80d14311c39f35f516fa664deaaaa13e85b2f7493f37f6144d86991ec012937307647bd3b9a82abe2974e1407241d54947bbb39763a4cac9f77166ad92a0@10.3.58.6:30303"
         />
+        <div className="mt-4">
+          <Toggle
+            id="boot_node"
+            label="Bootnode"
+            checked={isChecked}
+            onClick={() => setIsChecked(!isChecked)}
+          />
+        </div>
       </div>
     </div>
   );
