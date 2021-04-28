9f5b8be01bc91abe22269df69f32ac819af1ae69
ahmed-abdelhamid <slevin.glevra@gmail.com>
2021-04-28 22:29:18 +0300

    Extract FormLayout to its own component

--- pages/deployments/ethereum/nodes/create.tsx
+++ pages/deployments/ethereum/nodes/create.tsx
@@ -1,21 +1,14 @@
 import React from 'react'
 import Layout from '@components/templates/Layout/Layout'
+import FormLayout from '@components/templates/FormLayout/FormLayout'
 import CreateNodeForm from '@components/organisms/CreateNodeForm/CreateNodeForm'
 
 const CreateNode: React.FC = () => {
   return (
     <Layout>
-      <div className="py-6">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-          <h1 className="text-2xl font-semibold">Create New Node</h1>
-        </div>
-
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-          <div className="bg-white overflow-hidden shadow rounded-lg divide-y divide-gray-200 mt-4">
-            <CreateNodeForm />
-          </div>
-        </div>
-      </div>
+      <FormLayout title="Create New Node">
+        <CreateNodeForm />
+      </FormLayout>
     </Layout>
   )
 }
