f37593ce87c8b442b243d370a80ada3bdcffd4a5
ahmed-abdelhamid <slevin.glevra@gmail.com>
2021-04-29 22:48:57 +0300

    Create ipfs peer detail page

---
+++ pages/deployments/ipfs/peers/[peerName].tsx
@@ -0,0 +1,11 @@
+import Layout from '@components/templates/Layout/Layout'
+
+const IPFSPeerDetail: React.FC = () => {
+  return (
+    <Layout>
+      <h1>Peer Detail Page</h1>
+    </Layout>
+  )
+}
+
+export default IPFSPeerDetail
