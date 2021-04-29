bf5fcbc32bcbb1de98bd7e2d904945e473c1eec1
ahmed-abdelhamid <slevin.glevra@gmail.com>
2021-04-29 22:56:55 +0300

    Add layout to peer detail page

--- components/molecules/PageDetailsHeader/PageDetailsHeader.tsx
+++ components/molecules/PageDetailsHeader/PageDetailsHeader.tsx
@@ -1,12 +1,12 @@
 interface Props {
-  nodeName: string
+  title: string
   date: string
 }
 
-const PageDetailsHeader: React.FC<Props> = ({ nodeName, date }) => {
+const PageDetailsHeader: React.FC<Props> = ({ title, date }) => {
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-      <h1 className="text-2xl font-semibold">{nodeName}</h1>
+      <h1 className="text-2xl font-semibold">{title}</h1>
       <span className="text-xs text-gray-500">Last Updated on {date}</span>
     </div>
   )
--- pages/deployments/ethereum/nodes/[nodeName].tsx
+++ pages/deployments/ethereum/nodes/[nodeName].tsx
@@ -30,26 +30,26 @@ const EthereumNodeDetails: React.FC<Props> = ({ node }) => {
   // }
 
   // return (
-  //   <Layout>
-  //     <div className="py-6">
-  //       <PageDetailsHeader nodeName={node.name} date="January 11, 2021" />
+  // <Layout>
+  //   <div className="py-6">
+  //     <PageDetailsHeader nodeName={node.name} date="January 11, 2021" />
 
-  //       <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-  //         {/* Stats */}
-  //         <div>
-  //           <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-  //             <StatsComponent title="Last Block Number" content="11,971,897" />
-  //             <StatsComponent title="Transactions Today" content="1,024,569" />
-  //             <StatsComponent title="Connected Peers" content="21" />
-  //           </dl>
-  //         </div>
+  //     <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+  //       {/* Stats */}
+  //       <div>
+  //         <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+  //           <StatsComponent title="Last Block Number" content="11,971,897" />
+  //           <StatsComponent title="Transactions Today" content="1,024,569" />
+  //           <StatsComponent title="Connected Peers" content="21" />
+  //         </dl>
+  //       </div>
 
-  //         <div className="bg-white overflow-hidden shadow rounded-lg divided-y divided-gray-200 mt-4">
-  //           <Tabs node={node} />
-  //         </div>
+  //       <div className="bg-white overflow-hidden shadow rounded-lg divided-y divided-gray-200 mt-4">
+  //         <Tabs node={node} />
   //       </div>
   //     </div>
-  //   </Layout>
+  //   </div>
+  // </Layout>
   // )
 }
 
