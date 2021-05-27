018c5c23ea0ca2189dffba396dff0fa33eff9b98
ahmed-abdelhamid <slevin.glevra@gmail.com>
2021-05-27 11:16:21 +0300

    Add function to return all beacon nodes

---
+++ src/utils/requests/Ethereum2Requests.ts
@@ -0,0 +1,16 @@
+import axios from '../axios'
+
+import { Ethereum2BeaconNode } from '@interfaces/Ethereum2BeaconNode'
+
+/**
+ * Send a get request to find all ethereum 2 beacon nodes
+ * @returns All Ethereum 2.0 beacon nodes
+ */
+export const getAllEthereum2BeaconNodes = async (): Promise<
+  Ethereum2BeaconNode[]
+> => {
+  const { data } = await axios.get<{ beaconnodes: Ethereum2BeaconNode[] }>(
+    `/ethereum2/beaonnodes`
+  )
+  return data.beaconnodes
+}
