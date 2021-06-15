0773cfc8467e6c17cb88ce128b30ae86ce196425
ahmed-abdelhamid <slevin.glevra@gmail.com>
2021-06-15 10:59:55 +0300

    Fix border styling in linked tabs

--- components/organisms/LinkedTabs/LinkedTabs.tsx
+++ components/organisms/LinkedTabs/LinkedTabs.tsx
@@ -10,14 +10,14 @@ const Tabs: React.FC<Props> = ({ tabs }) => {
 
   return (
     // <div className="border-b border-gray-200">
-    <nav className="flex" aria-label="Tabs">
+    <nav className="flex divide-x-2" aria-label="Tabs">
       {tabs.map(({ name, href }) => (
         <Link key={name} href={href}>
           <a
             className={`${
               pathname === href
                 ? 'border-indigo-500 text-indigo-600 shadow'
-                : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
+                : 'text-gray-500 hover:text-gray-700'
             } first:rounded-tl-lg last:rounded-tr-lg whitespace-nowrap py-4 px-8 border-b-2 font-medium text-sm bg-white`}
             aria-current={pathname === href ? 'page' : undefined}
           >
