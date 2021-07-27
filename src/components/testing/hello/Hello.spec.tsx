f1c0cc9ea291dd72494fd0dc43888a903582c2af
ahmed-abdelhamid <slevin.glevra@gmail.com>
2021-07-27 15:16:07 +0300

    Remove junk components

--- src/components/testing/hello/Hello.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-import Hello from './Hello';
-
-it('render hello word', () => {
-  render(<Hello />);
-  const myElement = screen.getByText('Hello World');
-  expect(myElement).toBeInTheDocument();
-});
--- src/components/testing/hello/Hello.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Hello = () => {
-  return <div>Hello World</div>;
-};
-
-export default Hello;
