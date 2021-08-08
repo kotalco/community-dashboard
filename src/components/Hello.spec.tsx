45baebbb7557f8dfd50368b8f95978657876c059
ahmed-abdelhamid <slevin.glevra@gmail.com>
2021-08-08 16:42:08 +0300

    Remove unused components

--- src/components/Hello.spec.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { Hello } from './Hello';
-
-it('renders hello world', () => {
-  render(<Hello />);
-  const myElement = screen.getByText(/Hello World/);
-  expect(myElement).toBeInTheDocument();
-});
