7b2af1406cd8c177dfed99aa7c85adb26fd86ccc
ahmed-abdelhamid <slevin.glevra@gmail.com>
2021-10-13 02:25:45 +0300

    Fix timeout run only once

--- src/hooks/useLogs.ts
+++ src/hooks/useLogs.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 export const OPEN_CONNECTION_MSG = 'Connection Established.';
 export const CLOSE_CONNECTION_MSG = 'Disconnected. Connection Closed.';
@@ -9,9 +9,19 @@ const TIME_INTERVAL = 10000;
 
 export const useLogs = (pathname: string) => {
   const [logs, setLogs] = useState<string[]>([]);
+  const timeoutRef = useRef<NodeJS.Timeout>();
+  const websocketRef = useRef<WebSocket>();
 
-  const createWebsocket = useCallback(() => {
+  const connect = useCallback(() => {
+    console.log('run');
     const ws = new WebSocket(`${NEXT_PUBLIC_WS_BASE_URL}${pathname}`);
+    const check = () => {
+      if (
+        !websocketRef.current ||
+        websocketRef.current.readyState === WebSocket.CLOSED
+      )
+        connect();
+    };
 
     ws.onopen = () => {
       setLogs((logs) => {
@@ -20,6 +30,11 @@ export const useLogs = (pathname: string) => {
         }
         return [...logs, OPEN_CONNECTION_MSG];
       });
+
+      websocketRef.current = ws;
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
     };
 
     ws.onmessage = (event: MessageEvent<string>) => {
@@ -30,13 +45,6 @@ export const useLogs = (pathname: string) => {
         return [...logs, event.data];
       });
     };
-
-    return { ws };
-  }, [pathname]);
-
-  useEffect(() => {
-    const { ws } = createWebsocket();
-    let timeoutId: NodeJS.Timeout;
 
     ws.onclose = () => {
       setLogs((logs) => {
@@ -54,18 +62,23 @@ export const useLogs = (pathname: string) => {
         ];
       });
 
-      timeoutId = setTimeout(() => {
-        createWebsocket();
-      }, TIME_INTERVAL);
+      timeoutRef.current = setTimeout(check, TIME_INTERVAL);
     };
+  }, [pathname]);
 
+  useEffect(() => {
+    connect();
     return () => {
-      // eslint-disable-next-line @typescript-eslint/no-empty-function
-      ws.onclose = () => {};
-      ws.close();
-      if (timeoutId) clearTimeout(timeoutId);
+      if (websocketRef.current) {
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        websocketRef.current.onclose = () => {};
+        websocketRef.current.close();
+      }
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
     };
-  }, [createWebsocket, pathname]);
+  }, [connect]);
 
   return { logs };
 };
