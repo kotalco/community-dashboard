571a6ed1154f4356b6b6e05599be33766c012a7a
ahmed-abdelhamid <slevin.glevra@gmail.com>
2022-03-01 20:10:19 +0300

    Auth: Login form is added

--- src/components/molecules/TextInput/TextInput.tsx
+++ src/components/molecules/TextInput/TextInput.tsx
@@ -18,6 +18,7 @@ interface Props {
   disabled?: boolean;
   placeholder?: string;
   unit?: string;
+  fullWidth?: boolean;
 }
 
 const TextInput = React.forwardRef<HTMLInputElement, Props>(
@@ -34,6 +35,7 @@ const TextInput = React.forwardRef<HTMLInputElement, Props>(
       disabled,
       defaultValue,
       unit,
+      fullWidth,
     },
     ref
   ) => {
@@ -51,12 +53,15 @@ const TextInput = React.forwardRef<HTMLInputElement, Props>(
     return (
       <div className="mb-4">
         {label && <InputLabel htmlFor={name}>{label}</InputLabel>}
-        <fieldset disabled={disabled} className="relative max-w-xs">
+        <fieldset
+          disabled={disabled}
+          className={`relative ${fullWidth ? 'w-full' : 'max-w-xs'}`}
+        >
           <input
             ref={ref}
             type={inputType}
             id={name}
-            className={`shadow-sm focus:ring-indigo-500 block w-full sm:text-sm disabled:bg-gray-100 disabled:text-gray-500 rounded-md ${
+            className={`appearance-none shadow-sm focus:ring-indigo-500 block w-full sm:text-sm disabled:bg-gray-100 disabled:text-gray-500 rounded-md ${
               error ? 'border-red-300' : 'border-gray-300'
             }`}
             placeholder={placeholder}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,75 @@
-import Layout from '@components/templates/Layout/Layout';
+import { useForm } from 'react-hook-form';
+import Link from 'next/link';
+import { NextPage } from 'next';
+import { LockClosedIcon } from '@heroicons/react/solid';
 
-const Home: React.FC = () => {
+import TextInput from '@components/molecules/TextInput/TextInput';
+import Checkbox from '@components/molecules/CheckBox/CheckBox';
+import Button from '@components/atoms/Button/Button';
+
+const Login: NextPage = () => {
+  const { register } =
+    useForm<{ email: string; password: string; remember: boolean }>();
+
   return (
-    <Layout>
-      <h1>Index Page</h1>
-    </Layout>
+    <div className="flex flex-col justify-center h-screen py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md"></div>
+
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="px-4 py-8 bg-white shadow sm:rounded-lg sm:px-10">
+          <h2 className="mb-8 text-3xl font-extrabold text-center text-gray-900 font-nunito">
+            Sign in to your account
+          </h2>
+          <form className="space-y-6">
+            {/* Email */}
+            <TextInput fullWidth label="Email Address" {...register('email')} />
+
+            {/* Password */}
+            <TextInput
+              fullWidth
+              label="Password"
+              type="password"
+              {...register('password')}
+            />
+
+            <div className="flex items-center justify-between">
+              {/* Remember me */}
+              <div className="flex items-center">
+                <input
+                  type="checkbox"
+                  id="remember-me"
+                  name="remember-me"
+                  className="w-4 h-4 border-gray-300 rounded focus:ring-indigo-500"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="block ml-2 text-sm text-gray-900"
+                >
+                  Remember me
+                </label>
+              </div>
+              {/* Forget Password */}
+              <div className="text-sm">
+                <Link href="/forgot-password">
+                  <a className="font-medium text-indigo-600 hover:text-indigo-500">
+                    Forgot your password?
+                  </a>
+                </Link>
+              </div>
+            </div>
+
+            {/* Submit Button */}
+            <div>
+              <Button className="w-full text-sm btn btn-primary" type="submit">
+                <LockClosedIcon className="w-5 h-5 mr-32 text-indigo-800" />
+                Sign in
+              </Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 
-export default Home;
+export default Login;
