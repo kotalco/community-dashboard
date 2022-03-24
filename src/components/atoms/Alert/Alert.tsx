9d006af7da28b868e28d55c71f22f5ffd5f4197a
ahmed-abdelhamid <slevin.glevra@gmail.com>
2022-03-24 23:09:24 +0300

    Alert: Accept role attribute

--- src/components/atoms/Alert/Alert.tsx
+++ src/components/atoms/Alert/Alert.tsx
@@ -1,14 +1,31 @@
-import { ExclamationIcon } from '@heroicons/react/outline';
+import { CheckCircleIcon, ExclamationIcon } from '@heroicons/react/outline';
+import React, { PropsWithChildren } from 'react';
 
-const Alert: React.FC = ({ children }) => {
+interface Props {
+  role: 'warn' | 'sucess';
+}
+
+const Alert: React.FC<PropsWithChildren<Props>> = ({ children, role }) => {
   return (
-    <div className="max-w-lg p-4 text-sm rounded-md bg-yellow-50">
+    <div
+      className={`max-w-lg p-4 text-sm rounded-md ${
+        role === 'warn' ? 'bg-yellow-50' : 'bg-green-50'
+      }`}
+    >
       <div className="flex">
         <div className="shrink-0">
-          <ExclamationIcon
-            className="w-5 h-5 text-yellow-400"
-            aria-hidden="true"
-          />
+          {role === 'warn' && (
+            <ExclamationIcon
+              className="w-5 h-5 text-yellow-400"
+              aria-hidden="true"
+            />
+          )}
+          {role === 'sucess' && (
+            <CheckCircleIcon
+              className="w-5 h-5 text-green-400"
+              aria-hidden="true"
+            />
+          )}
         </div>
         <div className="ml-3">{children}</div>
       </div>
