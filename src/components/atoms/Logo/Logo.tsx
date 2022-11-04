809a9dae3b71126f972c5dbd30489a88fb016096
ahmed-abdelhamid <slevin.glevra@gmail.com>
2022-11-04 17:04:53 +0300

    ref: remove alpha from sidebar

--- src/components/atoms/Logo/Logo.tsx
+++ src/components/atoms/Logo/Logo.tsx
@@ -2,12 +2,9 @@ import Image from 'next/image';
 
 const Logo: React.FC<React.PropsWithChildren<unknown>> = () => {
   return (
-    <div className="shrink-0 flex items-center font-nunito">
+    <div className="flex items-center shrink-0 font-nunito">
       <Image src="/images/logo.svg" alt="logo" width={50} height={50} />
       <p className="ml-3 text-4xl font-bold">Kotal</p>
-      <sup className="text-sm text-gray-500 font-normal inline-block -mt-2 ml-1">
-        alpha
-      </sup>
     </div>
   );
 };
