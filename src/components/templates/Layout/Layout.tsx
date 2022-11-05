8e468d2cf940a8f9bbc6d575ed7a7fbfc15545aa
ahmed-abdelhamid <slevin.glevra@gmail.com>
2022-11-05 18:59:18 +0300

    ref: remove navbar

--- src/components/templates/Layout/Layout.tsx
+++ src/components/templates/Layout/Layout.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 
 import SidebarMobile from '@components/organisms/SidebarMobile/SidebarMobile';
 import SidebarDesktop from '@components/organisms/SidebarDesktop/SidebarDesktop';
-import Navbar from '@components/organisms/Navbar/Navbar';
 
 const Layout: React.FC<React.PropsWithChildren<unknown>> = ({ children }) => {
   // State for sidebar menu in mobile view
@@ -14,11 +13,11 @@ const Layout: React.FC<React.PropsWithChildren<unknown>> = ({ children }) => {
       <SidebarDesktop />
 
       <div className="flex flex-col flex-1 w-0 h-screen overflow-y-auto">
-        <div className="overflow-hidden">
+        {/* <div className="overflow-hidden">
           <Navbar setIsOpen={setIsOpen} />
-        </div>
+        </div> */}
 
-        <main className="z-10 flex-1 focus:outline-none" tabIndex={0}>
+        <main className="z-10 flex-1 mt-10 focus:outline-none" tabIndex={0}>
           <div className="h-full px-4 py-6 mx-auto max-w-7xl sm:px-6 md:px-8">
             {children}
           </div>
